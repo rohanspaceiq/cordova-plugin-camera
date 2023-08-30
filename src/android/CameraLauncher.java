072f396af6e9c2f2d6c5d7e23ff2a1473c34a46c
Rohan Kshirsagar <103414809+rohanspaceiq@users.noreply.github.com>
2023-08-30 17:07:17 +0530

    Removed the for loop which was rooe cause of Camera Launcher when targeting Android SDK 33

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -1313,12 +1313,6 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
 
     public void onRequestPermissionResult(int requestCode, String[] permissions,
                                           int[] grantResults) {
-        for (int r : grantResults) {
-            if (r == PackageManager.PERMISSION_DENIED) {
-                this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, PERMISSION_DENIED_ERROR));
-                return;
-            }
-        }
         switch (requestCode) {
             case TAKE_PIC_SEC:
                 takePicture(this.destType, this.encodingType);
